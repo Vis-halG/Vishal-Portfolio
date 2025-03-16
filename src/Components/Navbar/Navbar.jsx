6dbaedb93faa939df75d3a17ed2ea2fa2969fb76
Vishal Gupta <157860098+Vis-halG@users.noreply.github.com>
2025-03-16 22:58:32 +0530

    Update Navbar.jsx

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -55,7 +55,7 @@ const Navbar = () => {
           <a href="#">Home</a>
         </li>
         <li className="nav-item">
-          <a href="#projects_section">Projects</a>
+          <a href="#projects">Projects</a>
         </li>
        <li className="nav-item" id="skills">
   <a href="#skills_section">Tech Stack</a>
